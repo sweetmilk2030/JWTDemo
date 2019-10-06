4c58bf990c01504a7202d3680918cd1e9dc665ef
Hung Ha <sweetmilk2030@gmail.com>
2019-10-06 14:55:01 +0700

    set port 80

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import app from './app';
 
 const dbg = debug('jwtdemo:main');
 
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 80;
 
 (async () => {
   await app.listen(port);
