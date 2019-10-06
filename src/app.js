eae8fa5c934878b00f1f343851367c0a5053b87a
Hung Ha <sweetmilk2030@gmail.com>
2019-10-06 16:21:17 +0700

    connect to db

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
 import express from 'express';
 import bodyParser from 'body-parser'
+//import Role from '../models/role'
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  const {
-    name
-  } = req.body;
-  console.log(name);
+  const { Pool } = require('pg');
+  var config = {
+      user: 'zkhlbumxguqarf', 
+      database: 'df1k31nveubtst', 
+      password: 'd13c60e957966a96de0a011d1122e4d5642c484699ad572a179e731f8ae801cc', 
+      host: 'ec2-174-129-238-192.compute-1.amazonaws.com', 
+      port: 5432, 
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000,
+      dialect: 'postgres',
+  };
+  const pool = new Pool(config);
+  pool.on('error', function (err, client) {
+      console.error('idle client error', err.message, err.stack);
+  });
+  pool.query('SELECT * from "Roles" where id = ', ['1'], function(err, res) {
+      if(err) {
+          return console.error('error running query', err);
+      }
+      res.send(res.rows[0]);
+  });
   res.send('Hello World');
 })
 
