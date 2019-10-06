04ad3d72ec9687c179b000491e57d91c7cc60f4c
Hung Ha <sweetmilk2030@gmail.com>
2019-10-06 14:05:07 +0700

    Add seed roles

---
+++ db/seeders/20191006070218-role.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Roles', [{
+      name: 'User',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'PM',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Roles', null, {});
+  }
+};
