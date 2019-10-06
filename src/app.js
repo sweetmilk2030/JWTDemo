ee13edd75c9d1aaf186378cb3acc4810b753af2f
Hung Ha <sweetmilk2030@gmail.com>
2019-10-06 16:50:35 +0700

    connect db

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.post('/', function (req, res) {
   };
   const pool = new Pool(config);
   pool.on('error', function (err, client) {
-      console.error('idle client error', err.message, err.stack);
+    res.send('idle client error', err.message, err.stack);
   });
   pool.query('SELECT * from "Roles" where id = ', ['1'], function(err, res) {
       if(err) {
