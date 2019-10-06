2d1d1685bdf0bb0e2e30fbc484dddf16b3be4d58
Hung Ha <sweetmilk2030@gmail.com>
2019-10-06 16:54:48 +0700

    connect to db

--- src/app.js
+++ src/app.js
@@ -8,13 +8,10 @@ app.use(bodyParser.json());
 app.post('/', function (req, res) {
   const { Pool } = require('pg');
   var config = {
-      user: 'zkhlbumxguqarf', 
+      username: 'zkhlbumxguqarf', 
       database: 'df1k31nveubtst', 
       password: 'd13c60e957966a96de0a011d1122e4d5642c484699ad572a179e731f8ae801cc', 
-      host: 'ec2-174-129-238-192.compute-1.amazonaws.com', 
-      port: 5432, 
-      max: 10, // max number of clients in the pool
-      idleTimeoutMillis: 30000,
+      host: 'ec2-174-129-238-192.compute-1.amazonaws.com',
       dialect: 'postgres',
   };
   const pool = new Pool(config);
