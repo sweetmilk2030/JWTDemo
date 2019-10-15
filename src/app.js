0e497d781d0650fbacfa043eb21704e66e42d6b3
Hung Ha <sweetmilk2030@gmail.com>
2019-10-15 20:37:25 +0700

    'sql'

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  const { Pool } = require('pg');
+  const { Pool, Client } = require('pg');
+  const connectionString = 'postgres://zkhlbumxguqarf:d13c60e957966a96de0a011d1122e4d5642c484699ad572a179e731f8ae801cc@ec2-174-129-238-192.compute-1.amazonaws.com:5432/df1k31nveubtst';
   var config = {
       username: 'zkhlbumxguqarf', 
       database: 'df1k31nveubtst', 
@@ -14,18 +15,23 @@ app.post('/', function (req, res) {
       host: 'ec2-174-129-238-192.compute-1.amazonaws.com',
       dialect: 'postgres',
   };
-  const pool = new Pool(config);
-  pool.on('error', function (err, client) {
-    res.send('idle client error', err.message, err.stack);
+  const pool = new Pool({
+    connectionString: connectionString,
   });
-  pool.query('SELECT * from "Roles" where id = ', ['1'], function(err, res) {
-      if(err) {
-          res.send(err);
-      } else {
-        res.send(res.rows[0]);
-      }
-  });
-  res.send('Hello World Hung');
+  pool.query('SELECT NOW()', (err, res) => {
+    res.send(res);
+    pool.end();
+  })
+  const client = new Client({
+    connectionString: connectionString,
+  })
+  client.connect();
+
+  client.query('SELECT NOW()', (err, res) => {
+    res.send(res);
+    client.end();
+  })  
+  
 })
 
 export default app;
