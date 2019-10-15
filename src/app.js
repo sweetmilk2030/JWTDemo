6dcc75cc2d1e7ef5109ca5f04c57f23dcaea56fa
Hung Ha <sweetmilk2030@gmail.com>
2019-10-15 21:38:51 +0700

    'sql'

--- src/app.js
+++ src/app.js
@@ -1,26 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser'
-import pg from 'pg';
+const { Pool, Client } = require('pg')
 
 const app = express();
 app.use(bodyParser.json());
 const connectionString = 'postgres://zkhlbumxguqarf:d13c60e957966a96de0a011d1122e4d5642c484699ad572a179e731f8ae801cc@ec2-174-129-238-192.compute-1.amazonaws.com:5432/df1k31nveubtst';
 
 app.get('/', function (req, res, next) {
-  pg.connect(connectionString,function(err,client,done) {
-    if(err){
-        console.log("not able to get connection "+ err);
-        res.status(400).send(err);
-    } 
-    client.query('SELECT * FROM Roles where id = $1', [1],function(err,result) {
-        done(); // closing the connection;
-        if(err){
-            console.log(err);
-            res.status(400).send(err);
-        }
-        res.status(200).send(result.rows);
-    });
+  const pool = new Pool({
+    connectionString: connectionString,
   });
+
+  pool.query('SELECT * FROM Roles where id = $1', [1], (err, res) => {
+    if(err){
+      res.status(400).send(err);
+    }
+    res.status(200).send(res);
+    pool.end()
+  })
 });
 
 export default app;
